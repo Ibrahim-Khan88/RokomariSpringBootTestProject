485af90b8e77a8f569b0af0c79c1984534ba10c3
Ibrahim-Khan88 <ebrahimkhanobak@gmail.com>
2022-03-09 20:29:05 +0600

    Remove orginal audio from video and marge the input audio, Apply override file, Apply ffmpeg library

--- src/main/java/com/rokomarirpringboottestproject/RokomariSpringBootTestProject/service/DataService.java
+++ src/main/java/com/rokomarirpringboottestproject/RokomariSpringBootTestProject/service/DataService.java
@@ -11,80 +11,77 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-
-
 @Service
 public class DataService {
-	
-    String dir = new ClassPathResource("static/").getFile().getAbsolutePath();
-	
-	public DataService() throws IOException{
-		 
+
+	String dir = new ClassPathResource("static/").getFile().getAbsolutePath();
+
+	public DataService() throws IOException {
+
 	}
-	
-	
+
 	public String checkValidFun(MultipartFile[] files) {
-		
+
 		String file1Extension, file2Extension;
-		
-		if(files.length != 2) 
+
+		if (files.length != 2)
 			return "notValid";
-		
-		file1Extension = files[0].getOriginalFilename().substring(files[0].getOriginalFilename().indexOf(".")+1);
-		file2Extension = files[1].getOriginalFilename().substring(files[1].getOriginalFilename().indexOf(".")+1);
-		
-		
-		if(file1Extension.equals("mp3")) {
-			
-			if(file2Extension.equals("mp4"))
+
+		file1Extension = files[0].getOriginalFilename().substring(files[0].getOriginalFilename().indexOf(".") + 1);
+		file2Extension = files[1].getOriginalFilename().substring(files[1].getOriginalFilename().indexOf(".") + 1);
+
+		if (file1Extension.equals("mp3")) {
+
+			if (file2Extension.equals("mp4"))
 				return "FirstAudioFile";
 			else
 				return "notValid";
-			
-		}
-		else if(file1Extension.equals("mp4")){
-			
-			if(file2Extension.equals("mp3"))
+
+		} else if (file1Extension.equals("mp4")) {
+
+			if (file2Extension.equals("mp3"))
 				return "FirstVideoFile";
 			else
 				return "notValid";
-			
-		}
-		else 
+
+		} else
 			return "notValid";
 
 	}
-	
-	
-	public String margeAudioAndVideo(MultipartFile audioFile, MultipartFile videoFile) throws IOException, InterruptedException {
-		
-		String videoFilePath, videoWithOutAudioPath, audioFilePath, finalVideoFilePath, convertMkvFormatFilePath, ffmpeg;
+
+	public String margeAudioAndVideo(MultipartFile audioFile, MultipartFile videoFile)
+			throws IOException, InterruptedException {
+
+		String videoFilePath, videoWithOutAudioPath, audioFilePath, finalVideoFilePath, convertMkvFormatFilePath,
+				ffmpeg;
 		dir += File.separator;
 		ffmpeg = Loader.load(org.bytedeco.ffmpeg.ffmpeg.class);
-		 
-		videoFilePath = dir + "inputVideo" + videoFile.getOriginalFilename().substring(videoFile.getOriginalFilename().indexOf("."), videoFile.getOriginalFilename().length());
-		audioFilePath = dir + "inputAudio" + audioFile.getOriginalFilename().substring(audioFile.getOriginalFilename().indexOf("."), audioFile.getOriginalFilename().length());;
-		convertMkvFormatFilePath =  dir + "convertMkvFormat.mkv";
+
+		videoFilePath = dir + "inputVideo" + videoFile.getOriginalFilename()
+				.substring(videoFile.getOriginalFilename().indexOf("."), videoFile.getOriginalFilename().length());
+		audioFilePath = dir + "inputAudio" + audioFile.getOriginalFilename()
+				.substring(audioFile.getOriginalFilename().indexOf("."), audioFile.getOriginalFilename().length());
+		
+		convertMkvFormatFilePath = dir + "convertMkvFormat.mkv";
 		videoWithOutAudioPath = dir + "videoWithOutAudio.mkv";
 		finalVideoFilePath = dir + "video.mkv";
-		
-		
+
 		Files.copy(videoFile.getInputStream(), Paths.get(videoFilePath), StandardCopyOption.REPLACE_EXISTING);
 		Files.copy(audioFile.getInputStream(), Paths.get(audioFilePath), StandardCopyOption.REPLACE_EXISTING);
-		
-	     ProcessBuilder process1 = new ProcessBuilder(ffmpeg, "-y", "-i", videoFilePath, convertMkvFormatFilePath);
-	     process1.inheritIO().start().waitFor();
-	
-	     ProcessBuilder process2 = new ProcessBuilder(ffmpeg, "-y", "-i", convertMkvFormatFilePath, "-an", videoWithOutAudioPath);
-	     process2.inheritIO().start().waitFor();
-	     
-	     ProcessBuilder process3 = new ProcessBuilder(ffmpeg,  "-y", "-i", videoWithOutAudioPath, "-i", audioFilePath, finalVideoFilePath);
-	     process3.inheritIO().start().waitFor();
-	     
-	     return "video.mkv";
+
+		ProcessBuilder process1 = new ProcessBuilder(ffmpeg, "-y", "-i", videoFilePath, convertMkvFormatFilePath);
+		process1.inheritIO().start().waitFor();
+
+		ProcessBuilder process2 = new ProcessBuilder(ffmpeg, "-y", "-i", convertMkvFormatFilePath, "-an",
+				videoWithOutAudioPath);
+		process2.inheritIO().start().waitFor();
+
+		ProcessBuilder process3 = new ProcessBuilder(ffmpeg, "-y", "-i", videoWithOutAudioPath, "-i", audioFilePath,
+				finalVideoFilePath);
+		process3.inheritIO().start().waitFor();
+
+		return "video.mkv";
 
 	}
-
-	
 
 }
